9c6d73e522e15ecb0b198bfc529830b6f881ece9
shervin zadsoroor <shervinzadsoroor@gmail.com>
2019-12-22 02:37:39 +0330

    first commit

---
+++ src/main/java/confighibernate/HibernateUtil.java
@@ -0,0 +1,17 @@
+package confighibernate;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    static SessionFactory sessionFactory;
+
+    static {
+        sessionFactory = new Configuration().configure().buildSessionFactory();
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+}
